1507c8bb9e7dbcefe22a653c4b19ff49624c4ea4
taylor-c-williams <taylor.callan.williams@gmail.com>
2021-08-17 13:39:22 -0700

    button works

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 // import functions and grab DOM elements
+const remainingGuesses = document.getElementById('remaining-guesses');
+const guessInput = document.getElementById('guess-input');
+const guessButton = document.getElementById('guess-button');
+const result = document.getElementById('result');
+const winLose = document.getElementById('winlose');
 
 // initialize global state
+let guesses = 4;
+
 
 // set event listeners 
+guessButton.addEventListener('click', ()=>{
+    guesses --;
+    if (guesses < 1){
+        guesses = 0;
+    }
+    remainingGuesses.textContent = guesses;
+});
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
