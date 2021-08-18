fa8c0d72d8b8bfd68a30aa7404b147cabc01d072
taylor-c-williams <taylor.callan.williams@gmail.com>
2021-08-17 17:21:57 -0700

    formatting

--- app.js
+++ app.js
@@ -5,27 +5,28 @@ const guessButton = document.getElementById('guess-button');
 const result = document.getElementById('result');
 const resetButton = document.getElementById('reset-button');
 
-import { compareNumbers } from './utils.js';
-import { minMax } from './utils.js';
-import { tooHighLow } from './utils.js';
-import { disableButton } from './utils.js';
-import { randomNumber } from './utils.js';
-import { setNumber } from './utils.js';
- 
+import {
+    compareNumbers,
+    minMax,
+    tooHighLow,
+    disableButton,
+    randomNumber,
+    setNumber,
+} from './utils.js';
+
 let guesses = 4;
 let correctAnswer = randomNumber(1);
 console.log(correctAnswer);
 
 guessButton.addEventListener('click', () => {
-    // setNumber();
+	// setNumber();
     minMax();
-    guesses --;
-    let guessTry = compareNumbers(Number(userInput.value), (correctAnswer));
+    guesses--;
+    let guessTry = compareNumbers(Number(userInput.value), correctAnswer);
     tooHighLow(guessTry);
     remainingGuesses.textContent = guesses;
     disableButton(guesses, guessButton, guessTry);
 });
-
 
 // resetButton.addEventListener('click',() => {
 //   setNumber();
--- utils.js
+++ utils.js
@@ -5,24 +5,24 @@ export function minMax() {
     }
 }
 
-export function tooHighLow(aGuess){
-    if (aGuess === 0){
+export function tooHighLow(aGuess) {
+    if (aGuess === 0) {
         result.textContent = 'you won!';
     }
-    if (aGuess === 1){
+    if (aGuess === 1) {
         result.textContent = 'too high!';
     }
-    if (aGuess === -1){
+    if (aGuess === -1) {
         result.textContent = 'too low!';
     }
 }
 
-export function disableButton(guess, button, userGuess){
-    if (guess === 0){
+export function disableButton(guess, button, userGuess) {
+    if (guess === 0) {
         button.disabled = true;
         result.textContent = ('You lose!');
     }
-    if (userGuess === 0){
+    if (userGuess === 0) {
         button.disabled = true;
     }
 }
@@ -41,7 +41,7 @@ export function randomNumber(min) {
     return Math.floor(Math.random() * 20);
 }
 
-export function setNumber(){
+export function setNumber() {
     let correctAnswer = randomNumber(1);
     console.log(correctAnswer);
 }
